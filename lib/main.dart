993c027632c82eaaf5a58baa9eafee5d0046673e
pratik rai <pratik.rai@myparkplus.com>
2024-11-29 17:53:51 +0530

    navigation menu

--- lib/main.dart
+++ lib/main.dart
@@ -4,46 +4,94 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    ScreenA(),
+    ScreenB(data: 'Initial Data'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Layouts'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              color: Colors.blue,
-              width: double.infinity,
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                'Header',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Icon(Icons.star, size: 50, color: Colors.red),
-                  Icon(Icons.favorite, size: 50, color: Colors.pink),
-                  Icon(Icons.settings, size: 50, color: Colors.green),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Press Me'),
-              ),
-            ),
+        body: _screens[_currentIndex], // Show the current screen
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Screen A'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Screen B'),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ScreenA extends StatelessWidget {
+  const ScreenA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Screen A')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ScreenB(data: 'Hello from Screen A'),
+              ),
+            );
+          },
+          child: Text('Go to Screen B with Data'),
+        ),
+      ),
+    );
+  }
+}
+
+class ScreenB extends StatefulWidget {
+  final String data;
+
+  const ScreenB({super.key, required this.data});
+
+  @override
+  _ScreenBState createState() => _ScreenBState();
+}
+
+class _ScreenBState extends State<ScreenB> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter App')),
+      body: Center(
+        child: Text('Counter: $_counter', style: TextStyle(fontSize: 24)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: Icon(Icons.add),
       ),
     );
   }
